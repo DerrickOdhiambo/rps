cd7ac42225e1d9dbe10555039b523e5f2d2b1a6a
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-05-23 10:44:44 +0300

    import header and footer components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -11,7 +13,10 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main></main>
+      <main>
+        <Header />
+      </main>
+      <Footer />
     </div>
   );
 }
