bde7f9e25da6be87e3c40d1d2d2408616e5f7bcc
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-05-25 00:38:50 +0300

    pass props to the Rules component

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import styles from '../styles/Footer.module.css';
+import Rules from './Rules';
 
 const Footer = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <div className={styles.footer}>
-        <span>Rules</span>
+        <span onClick={() => setOpen(!open)}>Rules</span>
       </div>
+      <Rules
+        open={open}
+        setOpen={() => setOpen(!open)}
+        onClick={() => setOpen(!open)}
+      />
     </>
   );
 };
